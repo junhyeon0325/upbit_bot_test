05a60080329bd819083906e190c12e8bf6bcc6e3
junhyeon0325 <91251226+junhyeon0325@users.noreply.github.com>
2022-05-26 19:55:02 +0900

    텔레그렘 토큰 수정
    기존에 개인적으로 테스트하던 텔레그렘 토큰, chat_id는 개인이 사용하는 값을 입력하도록 수정
    이유는 키값을 반드시 안전한 곳에 별도로 보관해야한다는 홈페이지 설명으로 인한 이유로 수정

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 import telegram
 
-bot = telegram.Bot(token='5366296136:AAF9B_3YXH5fAEAefJDnkAJUC08gGTY1mX8')
-chat_id = 5342881340
+bot = telegram.Bot(token='텔레그렘 봇을 입력하시오')
+chat_id = 텔레그렘 chat_id를 입력하시오
 
 bot.sendMessage(chat_id=chat_id, text="보낼 메세지")
